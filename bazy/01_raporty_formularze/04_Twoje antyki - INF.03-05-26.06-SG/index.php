20a8a4a6a1f6d2865679c45be41382ff5e78deea
Jolanta Gryko <jgryko@gmail.com>
2026-09-22 09:49:00 +0200

    22.09

---
+++ bazy/01_raporty_formularze/04_Twoje antyki - INF.03-05-26.06-SG/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprzedaż antyków</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Najlepsze antyki w mieście</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>- Sofy -</h2>
+            <!-- skrypt 1 wariant 1a -->
+
+             <h2>- Fotele - </h2>
+             <!-- skrypt 1 wariant 1b -->
+
+             <h2>- Komody - </h2>
+             <!-- skrypt 1 wariant 1c -->
+
+             <!-- skrypt 2 -->
+        </section>
+
+        <aside>
+            <h2>Koszyk</h2>
+            <p>Zalogowano: Anna Kowalska</p>
+            <!-- Skrypt 3 -->
+        </aside>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: XYZ</p>
+    </footer>
+</body>
+</html>
